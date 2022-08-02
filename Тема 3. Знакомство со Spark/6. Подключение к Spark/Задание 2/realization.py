14e1a705afa92a33af3ff3bd282dea2bd5c7d102
Pavel Kulagov <kulagov@yandex-team.ru>
2022-08-02 22:45:22 +0300

    add de07030602

---
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 2/realization.py
@@ -0,0 +1,2 @@
+from pyspark.sql import SparkSession
+spark = # дополните код
---
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 2/submit.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import importlib
+
+import requests
+
+class TerminalColors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
+def submit(t_code, rlz_file=''):
+    user_code = ''
+    if rlz_file:
+        full_lesson_path = os.path.dirname(os.path.abspath(__file__))
+        user_file = f'{full_lesson_path}/{rlz_file}'
+
+        with open(user_file, 'r') as u_file:
+            user_code = u_file.read()
+
+    settings_path = os.path.dirname(os.path.abspath(__file__)).split('s7-lessons')[0]+'s7-lessons'
+    settings_file = f'{settings_path}/settings.py'
+    with open(settings_file) as settings:
+        user_settings = settings.read()
+
+    sys.path.append(settings_path)
+    u_settings = importlib.import_module('settings')
+    USER_HOST = u_settings.USER_HOST
+
+    print(f'{TerminalColors.OKGREEN}Для создания подключения потребуется некоторое время (до 2 минут)...{TerminalColors.ENDC}')
+    r = requests.post(
+        f'http://{USER_HOST}:3002',
+        json={
+            "code": user_code,
+            "test": t_code,
+            "conn": user_settings
+            })
+
+    print(r.json()['stderr'].replace('__test',rlz_file[:-3]))
+    print(r.json()['stdout'].replace('__test',rlz_file[:-3]))
+
+if __name__ == '__main__':
+    submit(
+        'de07030602',
+        'realization.py'
+    )
+
