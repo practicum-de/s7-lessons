98a62681db4cd9251f9a1bb5cb67b9c625f93ad1
Pavel Kulagov <kulagov@yandex-team.ru>
2022-08-03 01:12:39 +0300

    fix colors

--- Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/submit.py
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/submit.py
@@ -4,6 +4,17 @@ import importlib
 
 import requests
 
+class TerminalColors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
 def submit(t_code, rlz_file=''):
     user_code = ''
     if rlz_file:
